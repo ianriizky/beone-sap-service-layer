04312626e4c0dd6d9d857f1d5f91f97150888890
Septianata Rizky Pratama <ian.rizkypratama@gmail.com>
2022-05-07 15:05:39 +0700

    - Fix docblock

--- src/Services/SAPServiceLayer.php
+++ src/Services/SAPServiceLayer.php
@@ -9,8 +9,8 @@ use Illuminate\Support\Traits\Macroable;
 use RuntimeException;
 
 /**
- * @method \Illuminate\Http\Client\Response getChartOfAccountsId(string $id, array|string|null $query) Create "/ChartOfAccounts(:id)" GET request to the SAP service layer.
- * @method \Illuminate\Http\Client\Response getChartOfAccounts(array|string|null $query) Create "/ChartOfAccounts" GET request to the SAP service layer.
+ * @method \Illuminate\Http\Client\Response getChartOfAccountsId(string $id, array|string|null $query = null) Create "/ChartOfAccounts(:id)" GET request to the SAP service layer.
+ * @method \Illuminate\Http\Client\Response getChartOfAccounts(array|string|null $query = null) Create "/ChartOfAccounts" GET request to the SAP service layer.
  * @method \Illuminate\Http\Client\Response postChartOfAccounts(array $data = []) Create "/ChartOfAccounts" POST request to the SAP service layer.
  * @method \Illuminate\Http\Client\Response patchChartOfAccounts(string $id, array $data = []) Create "/ChartOfAccounts(:id)" PATCH request to the SAP service layer.
  * @method \Illuminate\Http\Client\Response deleteChartOfAccounts(string $id, array $data = []) Create "/ChartOfAccounts(:id)" DELETE request to the SAP service layer.
--- src/Support/Facades/SAPServiceLayer.php
+++ src/Support/Facades/SAPServiceLayer.php
@@ -6,8 +6,8 @@ use Ianriizky\BeoneSAPServiceLayer\Services\SAPServiceLayer as Service;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static \Illuminate\Http\Client\Response getChartOfAccountsId(string $id, array|string|null $query) Create "/ChartOfAccounts(:id)" GET request to the SAP service layer.
- * @method static \Illuminate\Http\Client\Response getChartOfAccounts(array|string|null $query) Create "/ChartOfAccounts" GET request to the SAP service layer.
+ * @method static \Illuminate\Http\Client\Response getChartOfAccountsId(string $id, array|string|null $query = null) Create "/ChartOfAccounts(:id)" GET request to the SAP service layer.
+ * @method static \Illuminate\Http\Client\Response getChartOfAccounts(array|string|null $query = null) Create "/ChartOfAccounts" GET request to the SAP service layer.
  * @method static \Illuminate\Http\Client\Response postChartOfAccounts(array $data = []) Create "/ChartOfAccounts" POST request to the SAP service layer.
  * @method static \Illuminate\Http\Client\Response patchChartOfAccounts(string $id, array $data = []) Create "/ChartOfAccounts(:id)" PATCH request to the SAP service layer.
  * @method static \Illuminate\Http\Client\Response deleteChartOfAccounts(string $id, array $data = []) Create "/ChartOfAccounts(:id)" DELETE request to the SAP service layer.
