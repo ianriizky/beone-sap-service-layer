803e53e8f55db1daca1e1437fffad55d314a9e47
Septianata Rizky Pratama <ian.rizkypratama@gmail.com>
2022-05-07 15:06:31 +0700

    - Finish api for JournalEntries module

---
+++ src/Services/Api/JournalEntries.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Ianriizky\BeoneSAPServiceLayer\Services\Api;
+
+use Illuminate\Http\Client\Response;
+
+/**
+ * This entity enables you to manipulate 'JournalEntries'.
+ * It represents journal transactions.
+ *
+ * @see https://sap-samples.github.io/smb-summit-hackathon/b1sl.html
+ *
+ * @property \Ianriizky\BeoneSAPServiceLayer\Http\Client\PendingRequest $request
+ */
+trait JournalEntries
+{
+    /**
+     * Create "/JournalEntries(:id)" GET request to the SAP service layer.
+     *
+     * Retrieve all or some selected properties from an instance of
+     * 'JournalEntries' with the given id.
+     *
+     * @param  string  $id
+     * @param  array|string|null  $query
+     * @return \Illuminate\Http\Client\Response
+     */
+    protected function getJournalEntriesId(string $id, $query = null): Response
+    {
+        return $this->request->get(sprintf('/JournalEntries(\'%s\')', $id), $query);
+    }
+
+    /**
+     * Create "/JournalEntries" GET request to the SAP service layer.
+     *
+     * Retrieve a collection of 'JournalEntries' with all or some
+     * selected properties in the given order by specifying
+     * the given filter condition.
+     *
+     * @param  array|string|null  $query
+     * @return \Illuminate\Http\Client\Response
+     */
+    protected function getJournalEntries($query = null): Response
+    {
+        return $this->request->get('/JournalEntries', $query);
+    }
+
+    /**
+     * Create "/JournalEntries" POST request to the SAP service layer.
+     *
+     * Create an instance of 'JournalEntries' with the given
+     * payload of type 'JournalEntry' in JSON format.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Http\Client\Response
+     */
+    protected function postJournalEntries(array $data = []): Response
+    {
+        return $this->request->post('/JournalEntries', $data);
+    }
+
+    /**
+     * Create "/JournalEntries(:id)" PATCH request to the SAP service layer.
+     *
+     * Update an instance of 'JournalEntries' with the given
+     * payload of type 'JournalEntry' in JSON format
+     * and with the specified id.
+     *
+     * @param  string  $id
+     * @param  array  $data
+     * @return \Illuminate\Http\Client\Response
+     */
+    protected function patchJournalEntries(string $id, array $data = []): Response
+    {
+        return $this->request->patch(sprintf('/JournalEntries(\'%s\')', $id), $data);
+    }
+
+    /**
+     * Create "/JournalEntries(:id)/Cancel" POST request to the SAP service layer.
+     *
+     * Invoke the method 'Cancel' on this EntitySet with the specified id.
+     *
+     * @param  string  $id
+     * @param  array  $data
+     * @return \Illuminate\Http\Client\Response
+     */
+    protected function postJournalEntriesCancel(string $id, array $data = []): Response
+    {
+        return $this->request->post(sprintf('/JournalEntries(\'%s\')/Cancel', $id), $data);
+    }
+}
--- src/Services/SAPServiceLayer.php
+++ src/Services/SAPServiceLayer.php
@@ -14,6 +14,11 @@ use RuntimeException;
  * @method \Illuminate\Http\Client\Response postChartOfAccounts(array $data = []) Create "/ChartOfAccounts" POST request to the SAP service layer.
  * @method \Illuminate\Http\Client\Response patchChartOfAccounts(string $id, array $data = []) Create "/ChartOfAccounts(:id)" PATCH request to the SAP service layer.
  * @method \Illuminate\Http\Client\Response deleteChartOfAccounts(string $id, array $data = []) Create "/ChartOfAccounts(:id)" DELETE request to the SAP service layer.
+ * @method \Illuminate\Http\Client\Response getJournalEntriesId(string $id, array|string|null $query = null) Create "/JournalEntries(:id)" GET request to the SAP service layer.
+ * @method \Illuminate\Http\Client\Response getJournalEntries(array|string|null $query = null) Create "/JournalEntries" GET request to the SAP service layer.
+ * @method \Illuminate\Http\Client\Response postJournalEntries(array $data = []) Create "/JournalEntries" POST request to the SAP service layer.
+ * @method \Illuminate\Http\Client\Response patchJournalEntries(string $id, array $data = []) Create "/JournalEntries(:id)" PATCH request to the SAP service layer.
+ * @method \Illuminate\Http\Client\Response postJournalEntriesCancel(string $id, array $data = []) Create "/JournalEntries(:id)/Cancel" POST request to the SAP service layer.
  * @method \Illuminate\Http\Client\Response Login(array $data) Create "/Login" POST request to the SAP service layer.
  *
  * @see https://sap-samples.github.io/smb-summit-hackathon/b1sl.html
@@ -24,6 +29,7 @@ class SAPServiceLayer
         __call as macroCall;
     }
     use Api\ChartOfAccounts;
+    use Api\JournalEntries;
     use Concerns\HandleAuthentication;
 
     /**
