a29de2fd93c65d1c7ef9a68268fbbcec7c94ce29
Septianata Rizky Pratama <ian.rizkypratama@gmail.com>
2022-05-07 16:56:27 +0700

    - Set separate for each api method comes from each traits on service and facade class docblock defintion

--- src/Services/SAPServiceLayer.php
+++ src/Services/SAPServiceLayer.php
@@ -9,16 +9,21 @@ use Illuminate\Support\Traits\Macroable;
 use RuntimeException;
 
 /**
+ * @see \Ianriizky\BeoneSAPServiceLayer\Services\Api\ChartOfAccounts
  * @method \Illuminate\Http\Client\Response getChartOfAccountsId(string $id, array|string|null $query = null) Create "/ChartOfAccounts(:id)" GET request to the SAP service layer.
  * @method \Illuminate\Http\Client\Response getChartOfAccounts(array|string|null $query = null) Create "/ChartOfAccounts" GET request to the SAP service layer.
  * @method \Illuminate\Http\Client\Response postChartOfAccounts(array $data = []) Create "/ChartOfAccounts" POST request to the SAP service layer.
  * @method \Illuminate\Http\Client\Response patchChartOfAccounts(string $id, array $data = []) Create "/ChartOfAccounts(:id)" PATCH request to the SAP service layer.
  * @method \Illuminate\Http\Client\Response deleteChartOfAccounts(string $id, array $data = []) Create "/ChartOfAccounts(:id)" DELETE request to the SAP service layer.
+ *
+ * @see \Ianriizky\BeoneSAPServiceLayer\Services\Api\JournalEntries
  * @method \Illuminate\Http\Client\Response getJournalEntriesId(string $id, array|string|null $query = null) Create "/JournalEntries(:id)" GET request to the SAP service layer.
  * @method \Illuminate\Http\Client\Response getJournalEntries(array|string|null $query = null) Create "/JournalEntries" GET request to the SAP service layer.
  * @method \Illuminate\Http\Client\Response postJournalEntries(array $data = []) Create "/JournalEntries" POST request to the SAP service layer.
  * @method \Illuminate\Http\Client\Response patchJournalEntries(string $id, array $data = []) Create "/JournalEntries(:id)" PATCH request to the SAP service layer.
  * @method \Illuminate\Http\Client\Response postJournalEntriesCancel(string $id, array $data = []) Create "/JournalEntries(:id)/Cancel" POST request to the SAP service layer.
+ *
+ * @see \Ianriizky\BeoneSAPServiceLayer\Services\Api\Login
  * @method \Illuminate\Http\Client\Response Login(array $data) Create "/Login" POST request to the SAP service layer.
  *
  * @see https://sap-samples.github.io/smb-summit-hackathon/b1sl.html
